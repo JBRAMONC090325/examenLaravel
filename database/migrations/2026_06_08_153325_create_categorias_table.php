685e45df41d7a603084a07c273482c1aad3408c6
Jorge Bramón Campos <jorge.bramon.campos@gmail.com>
2026-06-08 15:57:23 -0500

    Segundo commit examenLaravel

---
+++ database/migrations/2026_06_08_153325_create_categorias_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categorias', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', length:100);
+            $table->string('descripcion', length:150)->nullable();
+            $table->boolean('activo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categorias');
+    }
+};
